b9c62ee46c4a0c60dd1247571c72a4444911649c
jordanwarnes <jw@omg.lol>
2023-01-15 01:11:33 +0000

    Update user-overrides.js

--- user-overrides.js
+++ user-overrides.js
@@ -1,23 +1,6 @@
 user_pref("keyword.enabled", true);
 user_pref("extensions.pocket.enabled", false);
 user_pref("identity.fxaccounts.enabled", true);
-user_pref("places.history.enabled", false);
-user_pref("javascript.options.baselinejit", false);
-user_pref("layout.spellcheckDefault", 0);
-user_pref("signon.rememberSignons", false);
-
-user_pref("media.webspeech.synth.enabled", false);
-user_pref("media.navigator.enabled", false);
-
-user_pref("browser.search.suggest.enabled", false);
-user_pref("browser.urlbar.suggest.searches", false);
-user_pref("browser.compactmode.show", true);
 user_pref("browser.startup.page", 1);
 user_pref("browser.startup.homepage", "about:home");
 user_pref("browser.newtabpage.enabled", true);
-user_pref("browser.safebrowsing.downloads.remote.enabled", false);
-user_pref("browser.safebrowsing.phishing.enabled", false);
-user_pref("browser.safebrowsing.malware.enabled", false);
-user_pref("browser.urlbar.suggest.history", false);
-user_pref("browser.urlbar.suggest.openpage", false);
-user_pref("browser.urlbar.suggest.topsites", false);
